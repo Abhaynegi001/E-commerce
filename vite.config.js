9627fcbe720770bb9350890124302dc00fe4b3d2
Abhaynegi001 <abhaynegi691.gmail.com>
2025-04-06 20:05:56 +0530

    css

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure these paths are correct
+  ],
+  theme: {
+    extend: {},
+  },
 });
